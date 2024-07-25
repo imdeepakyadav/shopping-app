798c3a6ef636cd84f7ee775c1560093d5298ca7e
Deepak Yadav <99537696+imdeepakyadav@users.noreply.github.com>
2024-07-25 15:49:48 +0530

    Create com_allinone_online_shopping_store.php

---
+++ api/com_allinone_online_shopping_store.php
@@ -0,0 +1,455 @@
+{
+  "categories": [
+    {
+      "category_link": "https://bitli.in/zjDxr0z",
+      "category_name": "Electronic",
+      "category_image": "uploaded/categories/030924055333electronics.webp",
+      "open_in": "webview"
+    },
+    {
+      "category_link": "https://fktr.in/C10sA0r",
+      "category_name": "Mobiles",
+      "category_image": "uploaded/categories/030924060241mobiles.jpg",
+      "open_in": "webview"
+    },
+    {
+      "category_link": "https://myntr.it/2GWMmuj",
+      "category_name": "Women's Fashion",
+      "category_image": "uploaded/categories/032124072305womenfashion.jpg",
+      "open_in": "webview"
+    },
+    {
+      "category_link": "https://myntr.it/xH70urz",
+      "category_name": "Kids Wear",
+      "category_image": "uploaded/categories/032124073117kids.jpg",
+      "open_in": "webview"
+    },
+    {
+      "category_link": "https://myntr.it/G9nqbWj",
+      "category_name": "Men's Fashion",
+      "category_image": "uploaded/categories/032124071051menfashion.jpg",
+      "open_in": "webview"
+    },
+    {
+      "category_link": "https://bitli.in/uy9VKrn",
+      "category_name": "Home Decor",
+      "category_image": "uploaded/categories/030924055946home_furniture.jpg",
+      "open_in": "webview"
+    },
+    {
+      "category_link": "https://myntr.it/6ogqEde",
+      "category_name": "Beauty Care",
+      "category_image": "uploaded/categories/030924060813beauty.jpg",
+      "open_in": "webview"
+    }
+  ],
+  "banner_slider": [
+    {
+      "banner_link": "https://bit.ly/4b6YS6h",
+      "banner_title": "Ayurvedic Sugar-Forte Herbal",
+      "banner_image": "uploaded/banner/042624103146sugar-forte.jpg",
+      "open_in": "webview"
+    }
+  ],
+  "stores": [
+    {
+      "store_title": "Amazon",
+      "store_image": "uploaded/stores/030724113611Amazon-512.webp",
+      "store_url": "https://bitli.in/4zrhtl8",
+      "store_open": "webview",
+      "store_offer_desc": "Upto 50% off"
+    },
+    {
+      "store_title": "Flipkart",
+      "store_image": "uploaded/stores/0307241139171200x630wa.png",
+      "store_url": "https://fktr.in/dATgh37",
+      "store_open": "webview",
+      "store_offer_desc": "Upto 50% off"
+    },
+    {
+      "store_title": "Myntra",
+      "store_image": "uploaded/stores/030924070407myntra.png",
+      "store_url": "https://myntr.it/GVLuH4v",
+      "store_open": "webview",
+      "store_offer_desc": "Upto 50% off"
+    },
+    {
+      "store_title": "Ajio",
+      "store_image": "uploaded/stores/030924070925Ajio-MasterCard.jpg",
+      "store_url": "https://ajiio.in/BoG3jjZ",
+      "store_open": "webview",
+      "store_offer_desc": "Upto 50% off"
+    },
+    {
+      "store_title": "mamaearth",
+      "store_image": "uploaded/stores/032124063129mamaearth.png",
+      "store_url": "https://bitli.in/uG5jBTV",
+      "store_open": "webview",
+      "store_offer_desc": "Upto 50% off"
+    },
+    {
+      "store_title": "mCaffeine",
+      "store_image": "uploaded/stores/032124063351mcaffeine-store-4-1639372559.jpg",
+      "store_url": "https://bitli.in/UV0pwsE",
+      "store_open": "webview",
+      "store_offer_desc": "Upto 50% off"
+    },
+    {
+      "store_title": "Apple",
+      "store_image": "uploaded/stores/032124063617apple-store-1613721418.jpg",
+      "store_url": "https://bitli.in/9PA9t2V",
+      "store_open": "webview",
+      "store_offer_desc": "Upto 50% off"
+    },
+    {
+      "store_title": "WOW",
+      "store_image": "uploaded/stores/032124063739wow-skin.png",
+      "store_url": "https://bitli.in/q8zfOaA",
+      "store_open": "webview",
+      "store_offer_desc": "Upto 50% off"
+    },
+    {
+      "store_title": "Nykaa",
+      "store_image": "uploaded/stores/032124063831nykaabeauty.jpg",
+      "store_url": "https://bitli.in/lC6ZOnw",
+      "store_open": "webview",
+      "store_offer_desc": "Upto 50% off"
+    },
+    {
+      "store_title": "Clovia",
+      "store_image": "uploaded/stores/032124063923clovia-store.png",
+      "store_url": "https://bitli.in/eUqEkhM",
+      "store_open": "webview",
+      "store_offer_desc": "Upto 50% off"
+    },
+    {
+      "store_title": "Croma",
+      "store_image": "uploaded/stores/032124064018croma-ekaffise.jpg",
+      "store_url": "https://bitli.in/tDnxQ8K",
+      "store_open": "webview",
+      "store_offer_desc": "Upto 50% off"
+    }
+  ],
+  "strip_banner": [
+    {
+      "strip_banner_image": "uploaded/strip_banner/032124074126ajio-superseller-3902-1710959339.png",
+      "strip_banner_url": "https://ajiio.in/vqNrYZb",
+      "strip_banner_placement": "1",
+      "open_in": "webview"
+    },
+    {
+      "strip_banner_image": "uploaded/strip_banner/032124075257flipkartstrip.png",
+      "strip_banner_url": "https://fktr.in/d64yvxe",
+      "strip_banner_placement": "2",
+      "open_in": "webview"
+    }
+  ],
+  "electronic": [
+    {
+      "electronic_image": "uploaded/electronics/television.jpg",
+      "electronic_name": "Television",
+      "electronic_url": "https://bitli.in/uk5WrsR",
+      "electronic_desc": "Upto 50%Off",
+      "open_in": "webview"
+    },
+    {
+      "electronic_image": "uploaded/electronics/030824013018apple_iphone.jpg",
+      "electronic_name": "Iphone",
+      "electronic_url": "https://fktr.in/O1oKBYy",
+      "electronic_desc": "8% Off",
+      "open_in": "webview"
+    },
+    {
+      "electronic_image": "uploaded/electronics/032124100807lappi.png",
+      "electronic_name": "Laptops",
+      "electronic_url": "https://fktr.in/w5Hk2Nd",
+      "electronic_desc": "35% Off",
+      "open_in": "webview"
+    },
+    {
+      "electronic_image": "uploaded/electronics/032124100927tablet.png",
+      "electronic_name": "Tablets",
+      "electronic_url": "https://fktr.in/QLIvU1b",
+      "electronic_desc": "Upto 54%Off",
+      "open_in": "webview"
+    },
+    {
+      "electronic_image": "uploaded/electronics/032124100626computeracces.png",
+      "electronic_name": "Accessories",
+      "electronic_url": "https://bitli.in/RgSB9GC",
+      "electronic_desc": "Sale Live",
+      "open_in": "webview"
+    },
+    {
+      "electronic_image": "uploaded/electronics/030924064915smart_gadget.jpg",
+      "electronic_name": "Smart Gadget",
+      "electronic_url": "https://bitli.in/lVQAjA5",
+      "electronic_desc": "",
+      "open_in": "webview"
+    },
+    {
+      "electronic_image": "uploaded/electronics/030924065224printers.jpg",
+      "electronic_name": "Printers",
+      "electronic_url": "https://fktr.in/H9tWLfW",
+      "electronic_desc": "Upto 32% Off",
+      "open_in": "webview"
+    },
+    {
+      "electronic_image": "uploaded/electronics/030924065421camera.jpg",
+      "electronic_name": "Camera",
+      "electronic_url": "https://fktr.in/Ovs4cFf",
+      "electronic_desc": "Upto 25% Off",
+      "open_in": "webview"
+    }
+  ],
+  "top_deals": [
+    {
+      "topdeal_image": "uploaded/top_deals/030924094418suits.jpeg",
+      "topdeal_name": "Pakistani Suits",
+      "topdeal_url": "https://peachmode.com/collections/pakistani-suits",
+      "topdeal_desc": "Clearance Sale - Upto 40% Off",
+      "open_in": "webview"
+    },
+    {
+      "topdeal_image": "uploaded/top_deals/030924095911tshirt.jpeg",
+      "topdeal_name": "Tshirts",
+      "topdeal_url": "https://myntr.it/pVZrmf4",
+      "topdeal_desc": "Summer Sale 50% Off",
+      "open_in": "webview"
+    },
+    {
+      "topdeal_image": "uploaded/top_deals/032124102943footwear.jpeg",
+      "topdeal_name": "Footwear",
+      "topdeal_url": "https://myntr.it/kcpZ0Jm",
+      "topdeal_desc": "Discount Upto 65% Off",
+      "open_in": "webview"
+    },
+    {
+      "topdeal_image": "uploaded/top_deals/032124103805mobileaccessories.jpg",
+      "topdeal_name": "Mobile & Computer Accessories",
+      "topdeal_url": "https://bitli.in/lplnQQa",
+      "topdeal_desc": "Offer up to 30% Discount",
+      "open_in": "webview"
+    }
+  ],
+  "food": [
+    {
+      "food_image": "uploaded/food/030924112106Zomato_logo.png",
+      "food_name": "Zomato",
+      "food_url": "https://www.zomato.com/",
+      "open_in": "webview"
+    },
+    {
+      "food_image": "uploaded/food/030924112317Swiggy_logo.png",
+      "food_name": "Swiggy",
+      "food_url": "https://swiggy.com/",
+      "open_in": "webview"
+    },
+    {
+      "food_image": "uploaded/food/030924112748blinkit.webp",
+      "food_name": "Blinkit",
+      "food_url": "https://blinkit.com/",
+      "open_in": "webview"
+    },
+    {
+      "food_image": "uploaded/food/032124104153bb.webp",
+      "food_name": "Bigbasket",
+      "food_url": "https://www.bigbasket.com/",
+      "open_in": "webview"
+    },
+    {
+      "food_image": "uploaded/food/032124105101jio-mart-logo.png",
+      "food_name": "JioMart",
+      "food_url": "https://www.jiomart.com/c/groceries/2",
+      "open_in": "webview"
+    }
+  ],
+  "mens_wear": [
+    {
+      "mens_image": "uploaded/Mens/030924113604shirts.png",
+      "mens_name": "Shirts",
+      "mens_url": "https://www.amazon.in/Mens-Shirts/s?k=Men%27s+Shirts",
+      "open_in": "webview",
+      "mens_desc": "Starts From Rs499"
+    },
+    {
+      "mens_image": "uploaded/Mens/032124045653tshirt.jpg",
+      "mens_name": "T-Shirts",
+      "mens_url": "https://myntr.it/5A6rFqs",
+      "open_in": "webview",
+      "mens_desc": "Discount Upto 65% Off"
+    },
+    {
+      "mens_image": "uploaded/Mens/032124050259indian_ethnic.jpg",
+      "mens_name": "Indian Ethnic",
+      "mens_url": "https://myntr.it/oB9xK6D",
+      "open_in": "webview",
+      "mens_desc": "Best Offer Upto 40% Off"
+    },
+    {
+      "mens_image": "uploaded/Mens/032124051159bottomwear.png",
+      "mens_name": "Bottom Wear",
+      "mens_url": "https://myntr.it/ZH6912z",
+      "open_in": "webview",
+      "mens_desc": "Summer Sale 50% Off"
+    },
+    {
+      "mens_image": "uploaded/Mens/032124051422footwear.jpg",
+      "mens_name": "Footwear",
+      "mens_url": "https://myntr.it/NPRxx4u",
+      "open_in": "webview",
+      "mens_desc": "Starts From Rs. 199"
+    },
+    {
+      "mens_image": "uploaded/Mens/032124051720innerwear.png",
+      "mens_name": "Inner Wear",
+      "mens_url": "https://myntr.it/O4C9jam",
+      "open_in": "webview",
+      "mens_desc": "Starts From Rs. 99"
+    },
+    {
+      "mens_image": "uploaded/Mens/032124052000sportswear.jpg",
+      "mens_name": "Sports Wear",
+      "mens_url": "https://myntr.it/ej1jr3W",
+      "open_in": "webview",
+      "mens_desc": "Summer Sale 50% Off"
+    }
+  ],
+  "womens_wear": [
+    {
+      "womens_image": "uploaded/Womens/030924114507women_top.png",
+      "womens_name": "Western Wear",
+      "womens_url": "https://www.myntra.com/womens-western-wear",
+      "open_in": "webview",
+      "womens_desc": "Summer Sale 50% Off"
+    },
+    {
+      "womens_image": "uploaded/Womens/030924114657ethnic_wear.png",
+      "womens_name": "Ethnic Wear",
+      "womens_url": "https://myntr.it/Lc4aDf0",
+      "open_in": "webview",
+      "womens_desc": "Summer Sale 50% Off"
+    },
+    {
+      "womens_image": "uploaded/Womens/032124053550womenfootwear.jpg",
+      "womens_name": "Footwear",
+      "womens_url": "https://myntr.it/rAiPi27",
+      "open_in": "webview",
+      "womens_desc": "Starts From Rs. 499"
+    },
+    {
+      "womens_image": "uploaded/Womens/032124053959womeninner.jpg",
+      "womens_name": "Inner Wear",
+      "womens_url": "https://myntr.it/LzAfuwz",
+      "open_in": "webview",
+      "womens_desc": "Starts From Rs. 199"
+    },
+    {
+      "womens_image": "uploaded/Womens/032124054232beautycare.jpg",
+      "womens_name": "Beauty Care",
+      "womens_url": "https://myntr.it/8UAx1YH",
+      "open_in": "webview",
+      "womens_desc": "Discount Upto 65% Off"
+    },
+    {
+      "womens_image": "uploaded/Womens/032124054652womensportswear.jpg",
+      "womens_name": "Sports Active",
+      "womens_url": "https://myntr.it/WyV3LFS",
+      "open_in": "webview",
+      "womens_desc": "Starts From ₹ 199"
+    },
+    {
+      "womens_image": "uploaded/Womens/032124054904makeup.jpg",
+      "womens_name": "Makeup",
+      "womens_url": "https://myntr.it/4DZEeOd",
+      "open_in": "webview",
+      "womens_desc": "Discount Upto 45% Off"
+    },
+    {
+      "womens_image": "uploaded/Womens/032124055139skincare.jpg",
+      "womens_name": "Skin Care",
+      "womens_url": "https://myntr.it/I6SL9GO",
+      "open_in": "webview",
+      "womens_desc": "Discount Upto 25% Off"
+    },
+    {
+      "womens_image": "uploaded/Womens/032124055349haircare.jpg",
+      "womens_name": "Hair Care",
+      "womens_url": "https://myntr.it/ZOtbjLA",
+      "open_in": "webview",
+      "womens_desc": "Discount Upto 15% Off"
+    }
+  ],
+  "smartphone": [
+    {
+      "smartphone_image": "uploaded/Smartphone/030924115327mobile_under7999.jpg",
+      "smartphone_name": "Mobile Under Rs.7999",
+      "smartphone_url": "https://bitli.in/II5y0pe",
+      "open_in": "webview",
+      "smartphone_desc": "Clearance Sale - Upto 40% Off"
+    },
+    {
+      "smartphone_image": "uploaded/Smartphone/032124060221oneplus12.jpg",
+      "smartphone_name": "OnePlus 12 Rs. 69,999",
+      "smartphone_url": "https://bitli.in/bXt3mQj",
+      "open_in": "webview",
+      "smartphone_desc": "User Rating 4.5/5"
+    },
+    {
+      "smartphone_image": "uploaded/Smartphone/032124060747iphone15jpg.jpg",
+      "smartphone_name": "Iphone 15 Rs. 71,290",
+      "smartphone_url": "https://bitli.in/2lFF9Fb",
+      "open_in": "webview",
+      "smartphone_desc": "Discount 11% OFF"
+    }
+  ],
+  "app_setting": [
+    {
+      "topretailer_name": "Top Retailers",
+      "trending_name": "Trending Electronic Products",
+      "topdeal_name": "Top Deals",
+      "smartphone_name": "Smartphones - Best Deals",
+      "women_name": "Women's Fashion",
+      "men_name": "Men's Wear",
+      "food_name": "Food Corner",
+      "search_url": "https://www.amazon.in/s?k="
+    }
+  ],
+  "ad_setting": [
+    {
+      "ad_unit": "ca-app-pub-3940256099942544/9257395921",
+      "ad_type": "AppOpen",
+      "ad_network": "admob",
+      "ad_priority": "0",
+      "ad_counter": ""
+    },
+    {
+      "ad_unit": "ca-app-pub-3940256099942544/1033173712",
+      "ad_type": "Interstitial",
+      "ad_network": "admob",
+      "ad_priority": "0",
+      "ad_counter": "1"
+    },
+    {
+      "ad_unit": "ca-app-pub-3940256099942544/2247696110",
+      "ad_type": "Native",
+      "ad_network": "admob",
+      "ad_priority": "0",
+      "ad_counter": ""
+    },
+    {
+      "ad_unit": "ca-app-pub-3940256099942544/9214589741",
+      "ad_type": "Banner",
+      "ad_network": "admob",
+      "ad_priority": "0",
+      "ad_counter": ""
+    },
+    {
+      "ad_unit": "2110557905881155_2449510388652570",
+      "ad_type": "Banner",
+      "ad_network": "meta",
+      "ad_priority": "0",
+      "ad_counter": ""
+    }
+  ]
+}
